9472132a4c2a2111786c8d86eae16db05c8f5ce9
Kate <kate.s@tc-gaming.com>
2021-01-22 12:11:56 +0800

    init

---
+++ src/main/java/com/timesaver/luckyball/LuckyBallApplication.java
@@ -0,0 +1,13 @@
+package com.timesaver.luckyball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckyBallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckyBallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timesaver/luckyball/LuckyBallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timesaver.luckyball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyBallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
